f14c976931c68ab5445e5ec347fcf218fadfce39
xand.jpeg <125098050+xandejpeg@users.noreply.github.com>
2023-08-11 10:07:43 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <nav className="bg-slate-500 p-2 flex gap-3 items-end">
+      
+      <h1 className= "text=4xl text-zinc-100 font-bold uppercase">
+        Fiap Filmes CARAIO</h1>
+<ul>
+  <li>
+    <a href="#">Favoritos</a>
+  </li>
+</ul>
+    </nav>
+  )
+}
